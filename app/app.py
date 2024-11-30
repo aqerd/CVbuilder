6eb0e89f09d1d79f69cdccc053ccf96a49df5701
Rasul <rasulgasanbekov123@hmail.com>
2024-11-30 18:26:52 +0500

    feat: Added session support and used utils functions in download
    - Added session to store user data.
    - Used `create_pdf` and `create_docx` functions from utils in the `download` route.
    - Now files can be created and downloaded using stored data.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, send_file, request, session
 from utils import * 
+import os
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def index():
@@ -11,6 +14,8 @@ def profile():
     if request.method == 'POST':
         
         data = collect_data(request)
+
+        session['data'] = data
 
         return render_template('profile.html', projects=data.get("projects"), experiences=data.get("experiences"),
                                education=data.get("education"), languages=data.get("languages"), socials=data.get("socials"))
@@ -26,12 +31,16 @@ def samples():
 def export():
     return render_template('export.html')
 
-@app.route('/download/<file_type>')
+@app.route('/download/<file_type>',  methods=['GET'])
 def download(file_type):
+    data = session.get('data')  # Получаем данные из сессии
+
+    if not data:
+        return "No data available", 400
     if file_type == 'pdf':
-        filename = 'example.pdf'
+        filename = create_pdf(data)
     elif file_type == 'doc':
-        filename = 'example.doc'
+        filename = create_docx(data)
     elif file_type == 'jpg':
         filename = 'example.jpg'
     else:
