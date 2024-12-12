fd182c80b76a453fc0e2e64c41efade1cd7deea4
tazik <gor.prast@bk.ru>
2024-12-13 01:07:50 +0500

    add specialized method to send cv emails

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -14,3 +14,10 @@ def send_mail(subject, recipients, text_body, attachments=None):
                     msg.attach(attachment, mime_type, f.read())
         mail.send(msg)
         
+def send_cv_mail(recipient, name, cv_path):
+    send_mail(
+        subject='Ваше резюме',
+        recipients=[recipient],
+        text_body=render_template('cv_email.txt', name=name),
+        attachments=[cv_path]
+    )
