af37630f3d272c4ea391280f7e26de1b567fc4ac
tazik <gor.prast@bk.ru>
2024-12-13 01:19:44 +0500

    initialize mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_mail import Mail
+from config import Config
 import os
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.secret_key = os.urandom(24)
+mail = Mail(app)
 
 from app import routes
