b34116bd80fe6bc7af227521355f8bdb12814b09
tazik <gor.prast@bk.ru>
2024-12-19 00:21:22 +0500

    add some checks that format was selected

--- app/static/js/change_button.js
+++ app/static/js/change_button.js
@@ -1,24 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('button');
+    const buttons = document.querySelectorAll('.filetype');
+    const downloadBtn = document.getElementById('downloadBtn');
+    const emailBtn = document.getElementById('emailBtn');
+    const csrfToken = document.querySelector('input[name="csrf_token"]').value;
+    let selectedFormat = null;
 
     buttons.forEach(function(button) {
         button.addEventListener('click', function() {
-            if (button.classList.contains('current') || button.classList.contains('filetype')) {
-                buttons.forEach(function(btn) {
-                    btn.classList.remove('current');
-                });
-                button.classList.add('current');
-            }
+            buttons.forEach(function(btn) {
+                btn.classList.remove('current');
+            });
+            button.classList.add('current');
 
-            const format = button.getAttribute('data-format');
+            selectedFormat = button.getAttribute('data-format');
+            console.log('Выбранный формат:', selectedFormat);
+
             const xhr = new XMLHttpRequest();
             xhr.open('POST', '/set_format', true);
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.setRequestHeader('X-CSRF-Token', csrfToken);
             xhr.onreadystatechange = function() {
                 if (xhr.readyState === 4 && xhr.status === 200) {
                 }
             };
-            xhr.send('format=' + encodeURIComponent(format));
+            xhr.send('format=' + encodeURIComponent(selectedFormat));
         });
+    });
+
+    downloadBtn.addEventListener('click', function() {
+        if (!selectedFormat) {
+            alert('First select the format');
+        } else {
+            window.location.href = '/download';
+        }
+    });
+
+    emailBtn.addEventListener('click', function() {
+        if (!selectedFormat) {
+            alert('First select the format');
+        } else {
+            window.location.href = '/email';
+        }
     });
 });
