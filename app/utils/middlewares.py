76c9ecd71b82be28e484ea1512bfe84353b4c67f
tazik <gor.prast@bk.ru>
2024-12-19 11:16:41 +0500

    now returns error html

--- app/utils/middlewares.py
+++ app/utils/middlewares.py
@@ -5,6 +5,6 @@ def data_required(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if not session.get('data'):
-            return "No data available", 400
+            return render_template('error.html', msg_error='No data avaliable', error=400)
         return func(*args, **kwargs)
     return wrapper
