03b9fdbc75d92daa62554381372cdabe01047696
Ruslan Suleymanov <87182605+aqerd@users.noreply.github.com>
2024-12-21 19:24:12 +0500

    Merge remote-tracking branch 'origin/main'

--- app/utils/middlewares.py
+++ app/utils/middlewares.py
@@ -6,6 +6,6 @@ def data_required(func):
     def wrapper(*args, **kwargs):
         data = session.get('data')
         if not data or not any(value is not None and value != '' and value != [] for value in data.values()):
-            return render_template('error.html', msg_error='No data avaliable', error=400)
+            return render_template('error.html', msg_error='No data available', error=400)
         return func(data)
     return wrapper
