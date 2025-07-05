928899e71a0a5f59d187ca9e2672cbccdb1ec83d
Ruslan Suleymanov <2195486@gmail.com>
2025-07-05 22:29:27 +0500

    fixed 404 redirect

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from flask_wtf import CSRFProtect
 
-from app.routes import router
+from app.routes import page_not_found, router
 from settings import Settings
 
 site = Flask(__name__)
@@ -11,3 +11,4 @@ mail = Mail(site)
 csrf = CSRFProtect(site)
 site.register_blueprint(router)
 site.logger.setLevel(Settings.LOG_LEVEL)
+site.register_error_handler(404, page_not_found)
