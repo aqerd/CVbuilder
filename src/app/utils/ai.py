1cfb6a0fa57c8a6b8269ecb687a6a0a91d872cec
Ruslan Suleymanov <2195486@gmail.com>
2025-07-07 18:16:43 +0500

    css fix

--- src/app/utils/ai.py
+++ src/app/utils/ai.py
@@ -29,5 +29,5 @@ def generate_description(user_prompt, description_type):
         return data.get("result"), data.get("error_code"), data.get("error_message")
     except json.JSONDecodeError:
         return "", 1, "Error with decoding the response"
-    except Exception as e:
+    except Exception as _:
         return "", 1, "Error occurred with LLM service"
